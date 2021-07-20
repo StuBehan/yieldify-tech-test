513d4ba26014be9973a3c702cafbd62421516624
Stu Behan <33100049+StuBehan@users.noreply.github.com>
2021-07-20 16:04:51 +0100

    added random direction and rerender by interval

--- src/ball.js
+++ src/ball.js
@@ -1,11 +1,25 @@
 class Ball {
-  constructor (x, y) {
+  constructor (x, y, fps) {
     this.ballDia = 10
     this.ballX = x
     this.ballY = y
-    this.ballVelocityX = 0
-    this.ballVelocityY = 0
+    this.ballVelocityX = this.randomVelocity(fps)
+    this.ballVelocityY = this.randomVelocity(fps)
   }
 
-  
+  randomVelocity = (fps) => {
+    let randVelo = Math.floor(Math.random() * 100) / fps
+
+    if (Math.floor(Math.random() *2) == 0) {
+      return randVelo = -randVelo
+    } else {
+      return randVelo
+    }
+  }
+
+  moveBall = (displayAreaX, displayAreaY) => {
+    this.ballX += this.ballVelocityX
+    this.ballY += this.ballVelocityY
+  }
+
 }
--- src/displayArea.js
+++ src/displayArea.js
@@ -1,5 +1,7 @@
 class DisplayArea {
   constructor() {
+    this.balls = []
+    this.fps = 60
     this.canvas = document.getElementById('balls')
     this.contx = this.canvas.getContext('2d')
     this.initCanvas()
@@ -9,6 +11,7 @@ class DisplayArea {
     this.handleResize()
     window.addEventListener('resize', this.handleResize, false)
     window.addEventListener('click', this.handleClick, false)
+    setInterval(this.redraw, 1000 / this.fps)
   }
   
   handleResize = () => {
@@ -20,16 +23,24 @@ class DisplayArea {
   draw = () => {
     this.contx.beginPath()
     this.contx.strokeStyle = 'green'
-    this.contx.lineWidth= '5'
+    this.contx.lineWidth= '10'
     this.contx.strokeRect(0, 0, window.innerWidth, window.innerHeight)
   }
 
   handleClick = (event) => {
-    console.log(`click ${event.clientX} X ${event.clientY} Y`)
-    this.contx.fillStyle = 'red'
-    this.contx.beginPath()
-    this.contx.arc(event.clientX, event.clientY, 15, 0, Math.PI * 2)
-    this.contx.fill()
+    this.balls.push(new Ball(event.clientX, event.clientY, this.fps))
+  }
+
+  redraw = () => {
+    this.draw()
+    this.balls.forEach(ball => {
+      ball.moveBall(window.innerHeight, window.innerWidth)
+
+      this.contx.fillStyle = 'red'
+      this.contx.beginPath()
+      this.contx.arc(ball.ballX, ball.ballY, ball.ballDia, 0, Math.PI * 2)
+      this.contx.fill()
+    })
   }
 }
 
